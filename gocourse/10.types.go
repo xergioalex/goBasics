5ce27f90b75503ebd56d3bdc03b64bbdcc6f9018
Sergio Alexander <xergioalex@gmail.com>
2018-07-12 19:07:03 -0500

    Types

--- gocourse/10.types.go
+++ gocourse/10.types.go
@@ -4,13 +4,46 @@ import (
 	"fmt"
 )
 
-type Course struct {
+type CustomCourse struct {
 	Name string
 	Slug string
 	Skills [] string
 }
 
+// Course contiene los datos generales de un curso
+type Course struct {
+	Name        string
+	Description string
+	Slug        string
+	Teacher Instructor
+	Skills      []string
+}
+
+// Instructor contiene los datos de un instructor
+type Instructor struct {
+	FirstName     string
+	LastName      string
+	Twitter       string
+	ProfilePicURL string
+	Age           int
+	Country       string
+}
+
+// Career contiene los datos de una carrera
+type Career struct {
+	Name        string
+	Slug        string
+	Description string
+	Courses     []Course
+}
+
+
 func main() {
-	XergioAleX := Course{ Name: "Go", Slug: "go", Skills: [] string { "1", "2" } }
-	fmt.Println(XergioAleX)
+	goCourse := CustomCourse{ Name: "Go", Slug: "go", Skills: [] string { "1", "2" } }
+	fmt.Println(goCourse)
+
+	goCourse1 := new(CustomCourse)
+	goCourse1.Name = "Go1"
+	goCourse1.Slug = "go1"
+	goCourse1.Skills = [] string { "backend1" }
 }
